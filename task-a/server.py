d86324a6aedd222059660b429c14e56051085309
0xIbrahim T <85542378+0xIbrahim-t@users.noreply.github.com>
2024-07-01 20:13:57 +0530

    Update server.py

--- task-a/server.py
+++ task-a/server.py
@@ -1,2 +1,30 @@
 import socket
 import mysql.connector
+import threading
+
+mydb = mysql.connector.connect(host="db", db="game_db", user="username", password="password")
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind((socket.gethostbyname(socket.gethostname())), 6000)
+server_socket.listen()
+
+def client(connection, address):
+    print(f"{address} connected to the server")
+    connected = True
+    while connected:
+        message = connection.recv(1024).decode("utf-8")
+        if message == "Leave game":
+            connected = False
+            mycursor.close()
+            break
+        else :
+            mycursor = mydb.cursor()
+            mycursor.execute(message)
+            mydb.commit()
+    connection.close()
+        
+
+while True:
+    connection, address = server_socket.accept()
+    new_thread = threading.Thread(target=client, args=(connection, address))
+    new_thread.start()
