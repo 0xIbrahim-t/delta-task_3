9ebe236dee70cc860c556736f4455c9c39d36531
0xIbrahim T <85542378+0xIbrahim-t@users.noreply.github.com>
2024-07-09 12:58:09 +0530

    Update init.sql

--- task-a/init.sql
+++ task-a/init.sql
@@ -1,1 +1,11 @@
+CREATE DATABASE IF NOT EXISTS game_db;
 
+USE game_db;
+
+CREATE USER IF NOT EXISTS 'username' IDENTIFIED BY 'password';
+
+GRANT SELECT ON game_db.* TO 'username';
+FLUSH PRIVILEGES;
+
+create table users (username VARCHAR(10), password VARCHAR(1024), points INT);
+create table QA (question VARCHAR(1024), questionby VARCHAR(1024), answeredby VARCHAR(1024));
