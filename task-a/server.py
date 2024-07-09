3c7f498471392045ea06b78e70dd237333cbb48f
0xIbrahim T <85542378+0xIbrahim-t@users.noreply.github.com>
2024-07-09 12:29:31 +0530

    Update server.py

--- task-a/server.py
+++ task-a/server.py
@@ -13,14 +13,58 @@ def client(connection, address):
     connected = True
     while connected:
         message = connection.recv(1024).decode("utf-8")
-        if message == "Leave game":
+        mycursor = mydb.cursor()
+
+        if message == "exit":
             connected = False
             mycursor.close()
             break
-        else :
-            mycursor = mydb.cursor()
-            mycursor.execute(message)
+
+        elif message.split(",")[0] == "login":
+            mycursor.execute(f'SELECT password FROM users WHERE username = "{message.split(",")[1]}"')
+            password = mycursor.fetchone()[0]
+            connection.send(password.encode("utf-8"))
+
+        elif message.split(",")[0] == "signup":
+            mycursor.execute(f'SELECT username FROM users')
+            usernames = mycursor.fetchall()
+            usernames = [q[0] for q in usernames]
+            if message.split(",")[1] in usernames:
+                connection.send("username already exists".encode("utf-8"))
+            else :
+                mycursor.execute(f'INSERT INTO users (username, password, points) Values ("{message.split(",")[1]}", "{message.split(",")[2]}"), 0')
+                mydb.commit()
+                connection.send("Successfully signed up, you can now use that username and password to login to the game server!".encode("utf-8"))
+
+        elif message.split(",")[0] == "answer":
+            msg = ""
+            mycursor.execute(f"SELECT question FROM QA where questionby != '{message.split(",")[1]}'")
+            questions = mycursor.fetchall()
+
+            for question in questions:
+                mycursor.execute(f'SELECT answeredby FROM QA WHERE question = "{question[0]}"')
+                answeredby = mycursor.fetchone()[0].split(",")
+                if message.split(",")[1] not in answeredby:
+                    msg += question[0]
+                    msg += "~"
+
+            msg = msg[0:-1]
+            connection.send(msg.encode("utf-8"))
+            choice = connection.recv(1024).decode("utf-8")
+
+            if choice.split(",")[0] == "correct":
+                mycursor.execute(f'UPDATE users SET point = point + 1 WHERE username = "{message.split(",")[1]}"')
+
+            mycursor.execute(f'SELECT answeredby FROM QA WHERE question = "{choice.split(",")[1]}"')
+            updated = mycursor.fetchone()[0] + "," + message.split(",")[1]
+            mycursor.execute(f'UPDATE QA SET answeredby = {updated} WHERE question = "{choice.split(",")[1]}"')
             mydb.commit()
+
+        elif message.split(",")[0] == "question":
+            QA = connection.recv(1024).decode("utf-8")
+            mycursor.execute(f'INSERT INTO QA (questions, questionby, answeredby) Values ("{QA}", "{message.split(",")[1]}", "")')
+            mydb.commit()
+
     connection.close()
         
 
