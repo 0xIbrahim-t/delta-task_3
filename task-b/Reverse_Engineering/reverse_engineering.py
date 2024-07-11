a69f90539f37e64734e14f8a3098300dc0ea98c4
0xIbrahim T <85542378+0xIbrahim-t@users.noreply.github.com>
2024-07-11 23:51:54 +0530

    Update reverse_engineering.py

--- task-b/Reverse_Engineering/reverse_engineering.py
+++ task-b/Reverse_Engineering/reverse_engineering.py
@@ -1,1 +1,21 @@
+import subprocess
+import z3
 
+hardcoded_value = 1640
+
+string = z3.String('string')
+solver = z3.Solver()
+
+def str_to_val(stri):
+    result = subprocess.run([f"./string_to_value {stri}"], capture_output=True, text=True)
+    _result = result.stdout
+    return int(_result.split(",")[0])
+
+solver.add(str_to_val(string) == hardcoded_value)
+
+if solver.check() == z3.sat:
+    model = solver.model()
+    answer = model[string].as_string()
+    print(f"secret string found, string = '{answer}'")
+else:
+    print("No solution exists")
