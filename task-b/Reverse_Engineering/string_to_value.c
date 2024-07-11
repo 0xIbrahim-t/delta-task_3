49cd709793300b2da7fe0c8157b60d29f52d8f04
0xIbrahim T <85542378+0xIbrahim-t@users.noreply.github.com>
2024-07-11 20:20:57 +0530

    Update string_to_value.c

--- task-b/Reverse_Engineering/string_to_value.c
+++ task-b/Reverse_Engineering/string_to_value.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+#include <string.h>
 
+int string_to_value(char string[]){
+    int sum = 0;
+    for (int i = 0; i < strlen(string); i++) {
+        sum += (int)string[i];
+    }
+    return strlen(string) * sum;
+}
+
+int main(int argc, char* argv[]){
+    int flag_int = 1640;
+    printf("%d", string_to_value(argv[1]));
+    return 0;
+}
