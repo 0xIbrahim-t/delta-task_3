6910cd2e1b60b30fe78bc2674ffdd2098bc64751
0xIbrahim T <85542378+0xIbrahim-t@users.noreply.github.com>
2024-07-14 20:34:53 +0530

    Update init.sql

--- task-b/web/init.sql
+++ task-b/web/init.sql
@@ -1,4 +1,4 @@
 USE Task_db;
 
-create table web_users (username VARCHAR(20), password VARCHAR(20));
-INSERT INTO table_name (username, password) Values ("admin", "123456789");
+create table users (username VARCHAR(20), password VARCHAR(20));
+INSERT INTO users (username, password) Values ("admin", "123456789");
