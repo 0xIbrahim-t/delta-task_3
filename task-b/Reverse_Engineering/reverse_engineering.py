e71e630e38fdf41f66985d7a13ff700b7ed1b56c
0xIbrahim T <85542378+0xIbrahim-t@users.noreply.github.com>
2024-07-19 00:44:00 +0530

    Update reverse_engineering.py

--- task-b/Reverse_Engineering/reverse_engineering.py
+++ task-b/Reverse_Engineering/reverse_engineering.py
@@ -1,31 +1,30 @@
-import subprocess
 import z3
 
-hardcoded_value = 1640
+hardcoded_value = 99903
+len_of_word = 0
 
-chars = [z3.BitVec(f'char_{i}', 8) for i in range(4)]
-solver = z3.Solver()
+while True:
+    print(len_of_word)
+    if hardcoded_value % len_of_word != 0:
+        continue
 
-def str_to_val(string):
-    result = subprocess.run(["./string_to_value", string], capture_output=True, text=True)
-    return int(result.stdout.split(",")[0])
+    if hardcoded_value // (32*(len_of_word ** 2)) == 0:
+        break
 
-for char in chars:
-    solver.add(char >= 32, char <= 126)
+    chars = [z3.Int(f"char_{i}") for i in range(len_of_word)]
+    solver = z3.Solver()
 
-tested_strings = set()
+    for char in chars:
+        solver.add(char >= 32, char <= 126)
 
-while solver.check() == z3.sat:
-    model = solver.model()
-    candidate_string = ''.join([chr(model[char].as_long()) for char in chars])
-    
-    if candidate_string not in tested_strings:
-        tested_strings.add(candidate_string)
-    
-        if str_to_val(candidate_string) == hardcoded_value:
-            print(f"Secret string found: '{candidate_string}'")
-            solver.add(z3.Or([char != model[char] for char in chars]))
-        
-    solver.add(z3.Or([char != model[char] for char in chars]))
-else:
-    print("No solution exists")
+    squared_sum = z3.Sum([(i + 1) * (chars[i] * chars[i]) for i in range(len_of_word)])
+    solver.add(squared_sum * len_of_word == hardcoded_value)
+
+    while solver.check() == z3.sat:
+        model = solver.model()
+        print(''.join([chr(model[char].as_long()) for char in chars]))
+
+        for char in chars:
+            solver.add(char != model[char].as_long())
+
+print("End of the solutions")
