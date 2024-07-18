2967cd805ad76ebc4861e7e13de8633d0b95acd9
0xIbrahim T <85542378+0xIbrahim-t@users.noreply.github.com>
2024-07-19 00:45:11 +0530

    Update string_to_value.c

--- task-b/Reverse_Engineering/string_to_value.c
+++ task-b/Reverse_Engineering/string_to_value.c
@@ -4,13 +4,13 @@
 int string_to_value(char string[]){
     int sum = 0;
     for (int i = 0; i < strlen(string); i++) {
-        sum += (int)string[i];
+        sum += (int)string[i] * (int)string[i] * (i + 1);
     }
     return strlen(string) * sum;
 }
 
 int main(int argc, char* argv[]){
-    int flag_int = 1640;
+    int flag_int = 99903;
     if (string_to_value(argv[1]) == flag_int) {
         printf("%d, You have the found the flag", string_to_value(argv[1]));
     }
